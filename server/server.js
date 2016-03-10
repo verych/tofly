cd4e105d811450e3f705154f2030d25c9525fbcd
Verych <rveremyov@gmail.com>
2016-03-10 20:07:37 +0300

    stars, resize

--- server/server.js
+++ server/server.js
@@ -6,14 +6,157 @@ var io = io.listen(app);
 console.log('listening on *:8888');
 app.listen(8888);
 
+var field = {
+	w: 2000,
+	h: 1800,
+	d: 16
+};
+var stars = [];
+/*
+var stars = [
+	{x: 8, y: 100, z: 1, visible: true},
+	{x: 80, y: 300, z: 2, visible: true},
+	{x: 45, y: 14, z: 3, visible: false},
+	{x: 77, y: 225, z: 4, visible: true},
+	{x: 19, y: 500, z: 5, visible: true},
+	{x: 500, y: 45, z: 4, visible: true},
+	{x: 335, y: 11, z: 3, visible: true},
+	{x: 720, y: 224, z: 2, visible: false},
+	{x: 15, y: 422, z: 1, visible: true},
+	{x: 652, y: 67, z: 3, visible: true}
+];
+*/
+var clients = [];
+var sockets = {};
+var connectionsCount = 0;
+
+var generateStars = function() {
+	var getColor = function(level) {
+		var spread = 2;
+		var newLevel = field.d - level;
+		if(newLevel + spread >= field.d) {
+			newLevel = field.d - 1 - spread;
+		}
+
+	    var letters = '7899AABBBCCDDEEF'.split('');
+	    var color = '0x';
+	    for (var i = 0; i < 6; i++ ) {
+	        color += letters[Math.floor(Math.random() * (newLevel + spread))];
+	    }
+	    //debugger;
+	    return parseInt(color);
+	}
+
+	var count = 1000;
+	console.log('generating ' + count + ' stars');
+
+	for(var i = 0; i < count; i++){
+		var z = Math.round(Math.random() * field.d);
+		stars.push({
+			x: Math.round(Math.random() * field.w),
+			y: Math.round(Math.random() * field.h),
+			z: z,
+			color: getColor(z),
+			visible: true
+		});
+	}
+};
+generateStars();
+
+
+var mainClientLoop = function() {
+	for(var i = clients.length - 1; i >= 0; i--) {
+		tickPlayer(clients[i]);
+	}
+}
+
+var mainGameLoop = function() {
+	/*
+	for(var i = stars.length - 1; i >= 0; i--) {
+		stars[i].x -= (stars[i].z / 128);
+		if(stars[i].x < 0) {
+			stars[i].x = field.w;
+		}
+	}
+	*/
+}
+
+var tickPlayer = function(client) {
+	//console.log(client.name + " tick");
+	if(!client.viewport) {
+		//console.log('waiting for viewport data: ' + client.id);
+		return;
+	}
+
+	//tick data
+	var tickData = {stars: stars};
+	//me
+	client.x = client.viewport.w / 2;
+	client.y = client.viewport.h / 2;
+	//client.a += 0.1;
+
+	tickData.me = client;
+
+	//stars
+	sockets[client.id].emit('tick', tickData);
+
+	//clearInterval(mainClientLoopInterval);
+}
+
+var mainClientLoopInterval = setInterval(function(){
+	mainClientLoop();
+}, 10);
+
+var mainGameLoopInterval = setInterval(function(){
+	mainGameLoop();
+}, 10);
+
+
 io.sockets.on('connection', function (socket) {
-	console.log('new client is connected');
-	console.log(socket);
-	socket.on('eventServer', function (data) {
-		console.log(data.action);
-		socket.emit('eventClient', {'action': 'respawn', 'x': 5, 'y': 70});
+	connectionsCount++;
+	var currentPlayer = {
+        id: socket.id,
+        name: 'Flyer #' + connectionsCount,
+        x: null,
+        y: null,
+        a: Math.round(Math.random() * 360),
+        created: new Date().getTime(),
+        score: 1,
+        w: 10,
+        h: 50
+    };
+	sockets[socket.id] = socket;
+	clients.push(currentPlayer);
+	console.log('new ' + currentPlayer.name + ' is connected (' + currentPlayer.id + ')');
+
+	socket.on('respawn', function (data) {
+		socket.emit('respawn', {user: currentPlayer});
 	});
-	socket.on('disconnect', function () {
-		console.log('user disconnected');
+	socket.on('resize', function (data) {
+		currentPlayer.viewport = data;
+	});
+	socket.on('disconnect', function (data) {
+		var clientIndex = findIndex(clients, this.id);
+		sockets[this.id].emit('die', 'disconnect');
+		sockets[this.id].disconnect();
+		if(clientIndex >= 0) {
+			clients.splice(clientIndex, 1);
+		}
+		console.log('user ' + currentPlayer.name + ' disconnected (' + currentPlayer.id + ')' );
+		console.log(clientIndex);
 	});
 });
+
+//*****************utils*******************
+
+function findIndex(arr, id) {
+    var len = arr.length;
+
+    while (len--) {
+        if (arr[len].id === id) {
+            return len;
+        }
+    }
+
+    return -1;
+};
--- webclient/tofly.js
+++ webclient/tofly.js
@@ -5,8 +5,10 @@ var tofly = {
 		h: 350,
 		background: 0x010122,
 		me: {
+			/*
 			w: 10,
 			h: 50
+			*/
 		},
 		images: {
 			disk: {
@@ -21,6 +23,11 @@ var tofly = {
 $(function(){
 	tofly.initGraphics();
 	tofly.initSockets();
+	//adding resize handler
+	$(window).resize(function() {
+		//console.log(1);
+	  	tofly.resize();
+	});
 });
 
 tofly.initGraphics = function(){
@@ -29,6 +36,9 @@ tofly.initGraphics = function(){
 
 	// create the root of the scene graph
 	this.stage1 = new PIXI.Container();
+	// create the root of the scene graph
+	this.background = new PIXI.Container();
+	this.stage1.addChild(this.background);
 
 	// create a texture from an image path
 	var texture = PIXI.Texture.fromImage(this.config.images.disk.url);
@@ -39,8 +49,9 @@ tofly.initGraphics = function(){
 	// center the sprite's anchor point
 	this.me.anchor.x = 0.5;
 	this.me.anchor.y = 0.5;
-	this.me.scale.x = this.config.me.w / this.config.images.disk.w;
-	this.me.scale.y = this.config.me.h / this.config.images.disk.h;
+	this.me.kw = 1 / this.config.images.disk.w;
+	this.me.kh = 1 / this.config.images.disk.h;
+
 	// move the sprite to the center of the screen
 	this.me.position.x = this.config.w / 2;
 	this.me.position.y =  this.config.h / 2;
@@ -54,31 +65,72 @@ tofly.animate = function(){
 	requestAnimationFrame($.proxy(this.animate, this));
     // render the container
     this.renderer.render(this.stage1);
-
-
-    // just for fun, let's rotate mr rabbit a little
-    //this.me.rotation += 0.05;
 };
+
 
 tofly.initSockets = function (){
 	this.socket = io.connect('http://tofly:8888');
-	this.socket.on('eventClient', $.proxy(this.handleEvent, this));
-	this.socket.emit('eventServer', { data: {'action': 'respawn'}});
+	this.socket.on('respawn', $.proxy(this.doRespawn, this));
+	this.socket.on('tick', $.proxy(this.doTick, this));
+	this.resize();
+	this.socket.emit('respawn', { data: {'action': 'respawn'}});
 }
 
-tofly.handleEvent = function(data) {
-	debugger;
-	switch (data.action) {
-		case 'respawn':
-		this.doRespawn(data);
-		break;
-
-	}
-	console.log(data);
+tofly.doTick = function(data){
+	this.updateMe(data);
+	this.drawBackground(data.stars);
 }
 
 tofly.doRespawn = function(data){
-	this.me.position.x = data.x;
-	this.me.position.y = data.y;
+	/*
+	this.me.scale.x = this.me.kw * data.user.w;
+	this.me.scale.y = this.me.kh * data.user.h;
+	this.me.rotation = data.user.a;
+	*/
 	this.stage1.addChild(this.me);
 }
+
+tofly.drawBackground = function(stars) {
+	if(!this.background.children.length) {
+		for(var i = 0; i < stars.length; i++) {
+			var star = new PIXI.Graphics();
+			star.position.x = stars[i].x;
+			star.position.y = stars[i].y;
+
+			// set fill and line style
+			star.beginFill(stars[i].color);
+			//star.lineStyle(0.1, 0xeeeeff, 1);
+			star.drawCircle(0, 0, Math.ceil(stars[i].z / 8));
+			star.endFill();
+
+			this.background.addChild(star);
+		}
+	}
+	else if (this.background.children.length == stars.length) {
+		for(var i = 0; i < stars.length; i++) {
+			var star = this.background.children[i];
+			star.position.x = stars[i].x;
+			star.position.y = stars[i].y;
+		}
+	}
+	else {
+		console.log('stars problem');
+	}
+}
+
+tofly.updateMe = function(data) {
+	console.log(data.me);
+	this.me.position.x = data.me.x;
+	this.me.position.y = data.me.y;
+	this.me.scale.x = this.me.kw * data.me.w;
+	this.me.scale.y = this.me.kh * data.me.h;
+	this.me.rotation = data.me.a;
+}
+
+tofly.resize = function() {
+	//debugger;
+    this.renderer.resize(window.innerWidth, window.innerHeight);
+    this.socket.emit('resize', { w: window.innerWidth, h: window.innerHeight});
+}
+
+
